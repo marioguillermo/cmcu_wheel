d914e77be9adfe1cd79db490a2154691f7862829
Mario Guillermo <mario.guillermo950@gmail.com>
2019-08-08 09:37:09 -0500

    Change angle and string of names to display

--- js/roulette.js
+++ js/roulette.js
@@ -43,8 +43,8 @@ var rouletteModule = {
 	/**
 	 * Constants
 	 */
-	'NUMBER_OF_POCKETS': 30,
-	'DEGREE_OF_A_POCCKET': 12,
+	'NUMBER_OF_POCKETS': 17,
+	'DEGREE_OF_A_POCCKET': 21,
 	'LABEL_STYLE': {
 		font: "bold 18px Arial",
 		fill: "#fff",
@@ -68,7 +68,7 @@ var rouletteModule = {
 		if(labelStyle){
 			this.LABEL_STYLE = labelStyle;
 		}
-		var shuffledList = ['A1', 'B1', 'C1', 'D1', 'E1', 'A2', 'B2', 'C2', 'D2', 'E2', 'A3', 'B3', 'C3', 'D3', 'E3', 'A4', 'B4', 'C4', 'D4', 'E4', 'A5', 'B5', 'C5', 'D5', 'E5', 'A6', 'B6', 'C6', 'D6', 'E6'];
+		var shuffledList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17'];
     shuffledList = shuffle(shuffledList);
 		pocketBetStrip = pocketBetStrip || shuffledList;
 		this._buildRouletteUnit(rouletteID,pocketBetStrip);
